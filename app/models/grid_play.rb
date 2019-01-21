95d9e472c6859eb7f0c085d0689b64339ad691bf
MandoKapeso <mulabita@live.co.za>
2019-01-21 09:28:32 +0200

    Add GridPLay locgic to check when a game is nolonger in session

--- app/models/grid_play.rb
+++ app/models/grid_play.rb
@@ -14,10 +14,22 @@ module Grid
       [3, 5, 7],
    ]
 
-    def initilize()
+    def initilize
       @play_position = []
-      9.times.each { |idex| @play_position << index + 1}
+      9.times.each { |index| @play_position << index + 1}
       repopulate_grid
+      @game_in_session = true
+    end
+
+    def play
+      while @game_in_session
+        draw_grid
+        return unless check_player(@game_in_session)
+        player_one_play
+        return unless @game_in_session
+        player_two_play
+      end
+
     end
 
     def draw_grid
@@ -27,6 +39,7 @@ module Grid
           @grind[column].each do |row|
             puts row
           end
+        end
       rescue StandardError => e
         puts e.message
       end
@@ -34,19 +47,23 @@ module Grid
 
     def player_one_play
       puts "Player one : #{player_1.name} make selection"
-      @player_1.selection << play
+      @player_1.selection << select_option
+      @play_position[select_option.to_i] = "X"
       row_col = find_in_grid(player_selection)
       player_1.record_play(@game.id, row_col[:row], row_col[:column], "X")
+      check_player(@player_1)
     end
 
     def player_two_play
       puts "Player two : #{player_1.name} make selection"
-      @player_2.selection << play
+      @player_2.selection << select_option
+      @player_position[select_option.to_i] = "O"
       row_col = find_in_grid(player_selection)
       player_2.record_play(@game.id, row_col[:row], row_col[:column], "O")
+      check_player(@player_2)
     end
 
-    def play
+    def select_option
       selection = ''
       while true do
         begin
@@ -59,21 +76,34 @@ module Grid
             retry
           end
         rescue TypeError => e
-           puts "That's not a number. Try again"
-           retry ## restart from begin
+          puts "That's not a number. Try again"
+          ssretry ## restart from begin
         end
       end
       selection
     end
 
     def who_is_the_winner?
-      message = if check_player(@player_1) == @player_1.name
-          "Player #{@player_1.name} wins!!!" 
-        elsif check_player(@player_2) == @player_2.name
+      message = if check_player(@player_1)
+          "Player #{@player_1.name} wins!!!"
+        elsif check_player(@player_2)
           "Player #{@player_2.name} wins!!!"
         else
           "Game has ended in a tie!!!"
         end
+    end
+
+    private
+    def check_player(player)
+      @game_in_session = true
+      return @game_in_session if (player.game_selections.count < 3) #number of combinations to win a game have not been met therefore return false
+      WINNING_COMBINATIONS.each do |combination|
+        if combination == player.game_selections
+          @game_in_session = false
+          break
+        end
+      end
+      @game_in_session
     end
 
     def repopulate_grid
