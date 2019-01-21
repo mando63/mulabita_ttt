73a1f1b2fa0dbc4073b2ce388a55864859d202a8
MandoKapeso <mulabita@live.co.za>
2019-01-21 09:38:27 +0200

    Add logic to confirm winner

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,6 +1,6 @@
 class GamePlayer < ActiveRecord::Base
   # this model stores details of a user playing a game
-  attr_accessor :game_selections
+  attr_accessor :game_selections, :winner
   has_many :caper
 
   def game_selections
@@ -11,10 +11,13 @@ class GamePlayer < ActiveRecord::Base
     Caper.create!(
       game_id: game_id,
       player_id: self.id,
-      cell_row:, cell_row,
+      cell_row: cell_row,
       cell_column: cell_column,
       cell_state: cell_state
     )
   end
 
+  def has_won?(winner)
+    @winner = winner
+  end
 end
